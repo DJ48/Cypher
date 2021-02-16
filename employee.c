c92b494f0944c65b7a6d62801590962298294429
Sindhura1004 <79127764+Sindhura1004@users.noreply.github.com>
2021-02-16 12:26:05 +0530

    Update employee.c
    update changes with segmentation error

--- employee.c
+++ employee.c
@@ -1,85 +1,68 @@
-#include <stdio.h>
+/******************************************************************************
 
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
 #include <string.h>
+#include<stdlib.h>
 
 struct emp // new user defined datatype
-
 {
-
-char id[10];
-
-char name[50];
+    char id[10];
+    char name[50];
 
 };
 
 int main()
-  
-
 {
+    struct emp employees[3]; // struct emp vs emp1
+    printf("Enter 3 employee records id and name \n");
+    
+    for (int i = 0; i < 3; i++) 
+    {
+        scanf("%s",employees[i].id);
+        scanf("%s",employees[i].name);
+    }
 
-int dindex,f2;
-char d_id[10]
-struct emp employees[3]; // struct emp vs emp1
+    for (int i = 0; i < 3; i++) 
+    {
+        printf("%s\t",employees[i].id);
+        printf("%s\n",employees[i].name);
+    }
 
-printf("Enter 3 employee records id and name \n");
+    int f3;
+    char u_id[10];
+    printf ("Enter the Employee id that you want to update:-");
+    scanf ("%s", &u_id);
+    for (int i = 0; i < 3; i++)
+    {
+        if (strcmp (u_id, employees[i].id) == 0)
+        {
+            printf ("\n----Please Enter the new id and new name:-\n");
+            scanf ("%s", employees[i].id);
+            scanf ("%s", employees[i].name);
+            f3=1;
 
-for (int i = 0; i < 3; i++) {
-
-scanf("%s",employees[i].id);
-
-scanf("%s",employees[i].name);
-
-}
-
-for (int i = 0; i < 3; i++) {
-
-printf("%s\t",employees[i].id);
-
-printf("%s\n",employees[i].name);
-
-}
-printf ("Enter the id that you want to delete:");
-
-scanf ("%s", &d_id);
-
-for (int i = 0; i < 3; i++)
-{
-printf("Drebug");
-if (strcmp(d_id, employees[i].id) == 0)
-{
-
-dindex = i;
-f2=1;
-break;
-}
-
-}
-printf("%d",dindex);
-if(f2==0){
-printf("\nEmployee id %s not found.",d_id);
-}
-else{
-for (int i = dindex; i < 3; i++)
-{
-
-if (i == 2)
-{
-
-strcpy (employees[i].id, "\0");
-
-strcpy (employees[i].name, "\0");
-
-}
-
-else
-{
-
-employees[i] = employees[i + 1];
-
-}
-
+        }
+    }
+    if(f3==0)
+    {
+        printf("\nEmployee id %s not found.",u_id);
+    }
+    else
+    {
+        printf("\n-------Printing Details after update--------\n");
+        for (int i = 0; i < 3; i++)
+        {
+            printf ("%s\t", employees[i].id);
+            printf ("%s\n", employees[i].name);
+        }
+    }
+ 
 return 0;
+}
 
-}
-}
-}
