9b4d42cf826087e91487bce5bed892a8404acac6
Kiran445 <79127684+Kiran445@users.noreply.github.com>
2021-02-16 11:49:37 +0530

    Update employee.c
    committed by kiran

--- employee.c
+++ employee.c
@@ -1,1 +1,41 @@
 #include <stdio.h>
+
+#include <string.h>
+
+struct emp // new user defined datatype
+
+{
+
+char id[10];
+
+char name[20];
+
+};
+
+int main()
+
+{
+
+struct emp employees[3]; // struct emp vs emp1
+
+printf("Enter 3 employee records id and name \n");
+
+for (int i = 0; i < 3; i++) {
+
+scanf("%s",employees[i].id);
+
+scanf("%s",employees[i].name);
+
+}
+
+for (int i = 0; i < 3; i++) {
+
+printf("%s\t",employees[i].id);
+
+printf("%s\n",employees[i].name);
+
+}
+
+return 0;
+
+}
